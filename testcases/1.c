72e178600b72c7753c84db89844ad85c901fcca4
rollsafe <rollsafe@users.noreply.github.com>
2019-02-10 00:05:30 -0500

    Basic outline of the compiler

---
+++ testcases/1.c
@@ -0,0 +1,20 @@
+typedef struct {
+} shit;
+
+typedef int uintptr_t;
+
+uintptr_t value;
+
+
+int value3 = sizeof(int);
+
+int value4 = (short)6;
+
+int main(int argc, char** argv) {
+    typedef int int2;
+    int x = 3*(5+5),y;
+    if (1+1 == 2) {
+        y = 3;
+    }
+    return value*x;
+}
