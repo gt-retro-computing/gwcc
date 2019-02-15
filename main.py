19e270aee82948afacdd7c2b5f78a70bc2ea6399
rollsafe <rollsafe@users.noreply.github.com>
2019-02-15 02:59:35 -0500

    Extreme refactor main
    Wow

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from pycparser import parse_file, c_parser
 import gwcc
 from gwcc.abi.lc3 import LC3
 
-print 'The Gangweed Retargetable C Compiler [Version %s]' % (gwcc.__version__,)
-print '(c) 2019 gangweed ganggang. All rights resreved.'
-print
+def banner():
+    print 'The Gangweed Retargetable C Compiler [Version %s]' % (gwcc.__version__,)
+    print '(c) 2019 gangweed ganggang. All rights reserved.'
+    print
 
-parser = c_parser.CParser()
-ast = parse_file('testcases/1.c', use_cpp=True)
-compiler = gwcc.Compiler(LC3)
-compiler.compile(ast)
+if __name__ == '__main__':
+    banner()
+
+    parser = c_parser.CParser()
+    ast = parse_file('testcases/1.c', use_cpp=True)
+    compiler = gwcc.Compiler(LC3)
+    compiler.compile(ast)
